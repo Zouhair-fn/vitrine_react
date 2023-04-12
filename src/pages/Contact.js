2833bf405959bba8c462d2a5bade6f4c6839c9e7
zouhair <zoufan9@gmail.com>
2023-04-13 00:52:28 +0200

    Complètement de la page contact, ajout du formulaire et de la bibliothèque copy-to-clipboard

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React from "react";
+import Mouse from "../components/Mouse";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+import ContactForm from "../components/ContactForm";
+import CopyToClipboard from "react-copy-to-clipboard";
 
 const Contact = () => {
-    return (
-        <div>
-            <h1>Contact</h1>
+  return (
+    <main>
+      <Mouse />
+      <div className="contact">
+        <Navigation />
+        <Logo />
+        <ContactForm />
+        <div className="contact-infos">
+          <div className="address">
+            <div className="content">
+              <h4>adresse</h4>
+              <p>07 rue Laplace</p>
+              <p>06000</p>
+            </div>
+          </div>
+          <div className="phone">
+            <div className="content">
+              <h4>téléphone</h4>
+              <CopyToClipboard text="0606560854" className="over">
+                <p
+                  style={{ cursor: "pointer" }}
+                  className="clipboard"
+                  onClick={() => alert("Téléphone coplié !")}
+                >
+                  06 06 56 08 54
+                </p>
+              </CopyToClipboard>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </main>
+  );
 };
 
 export default Contact;
