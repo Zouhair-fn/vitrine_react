b235cdf590e7236efa8bec2114354703125b4614
zouhair <zoufan9@gmail.com>
2023-05-01 11:47:58 +0200

    build commit

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -7,7 +7,7 @@ export const projectsData = [
     infos:
       "J'ai créé un site web avec React, en utilisant une API pour récupérer les données des pays. La technologie React m'a permis de créer une interface utilisateur dynamique et conviviale. L'utilisation de l'API a permis de récupérer des données en temps réel et d'afficher des informations précises sur les pays. Le résultat est un site web fonctionnel, esthétique et facile à utiliser. La combinaison de React et de l'API a été une expérience enrichissante pour moi en tant que développeur.",
     img: "./assets/img/projet-2.jpg",
-    link: "http://www.google.com",
+    link: "https://countries-zouhair.netlify.app",
   },
   {
     id: 2,
@@ -17,7 +17,7 @@ export const projectsData = [
     infos:
       "J'ai créé un portfolio en ligne avec l'utilisation de : React et Tailwind CSS. J'ai utilisé ces deux technologies pour créer un site web élégant.",
     img: "./assets/img/projet-1.jpg",
-    link: "http://www.google.com",
+    link: "https://portfolio-zouhair.netlify.app",
   },
   {
     id: 3,
@@ -27,6 +27,6 @@ export const projectsData = [
     infos:
       "J'ai développé une petite application Node.js qui permet d'effectuer des opérations CRUD (Create, Read, Update, Delete) sur une base de données.",
     img: "./assets/img/projet-3.jpg",
-    link: "http://www.google.com",
+    link: "https://github.com/Zouhair-fn/node.jsApp",
   },
 ];
